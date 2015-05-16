96ef086f60f0253b8edaaf9410eede6ef865e3c9
David Greenhalgh <davidrgreenhalgh@gmail.com>
2015-05-16 17:43:00 -0400

    Added LinearLayoutManagerSampleActivity

--- SimpleItemDecorationSample/app/src/main/java/com/dgreenhalgh/android/simpleitemdecorationsample/MainActivity.java
+++ SimpleItemDecorationSample/app/src/main/java/com/dgreenhalgh/android/simpleitemdecorationsample/MainActivity.java
@@ -1,59 +1,33 @@
 package com.dgreenhalgh.android.simpleitemdecorationsample;
 
 import android.app.Activity;
-import android.graphics.drawable.Drawable;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-
-import com.dgreenhalgh.android.simpleitemdecoration.DividerItemDecoration;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
 
-    private RecyclerView mRecyclerView;
+    private Button mLinearLayoutManagerSampleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.activity_main_recyclerView);
-        initRecyclerView();
+        mLinearLayoutManagerSampleButton = (Button) findViewById(R.id.activity_main_linearLayoutManagerButton);
+        mLinearLayoutManagerSampleButton.setOnClickListener(mLinearLayoutManagerSampleButtonClickListener);
     }
 
-    private void initRecyclerView() {
-        List<String> sampleStringList = getSampleData();
+    private View.OnClickListener mLinearLayoutManagerSampleButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Context context = view.getContext();
 
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.setAdapter(new SimpleItemDecorationSampleListAdapter(sampleStringList));
-
-        addDividers();
-    }
-
-    private void addDividers() {
-        Drawable dividerDrawable = getResources().getDrawable(R.drawable.divider_sample);
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(dividerDrawable));
-    }
-
-    private List<String> getSampleData() {
-        List<String> sampleStringList = new ArrayList<>();
-        sampleStringList.add("Paul");
-        sampleStringList.add("David");
-        sampleStringList.add("Kristin");
-        sampleStringList.add("Chris");
-        sampleStringList.add("Josh");
-        sampleStringList.add("Andrew");
-        sampleStringList.add("Brian");
-        sampleStringList.add("Matt");
-        sampleStringList.add("Bill");
-        sampleStringList.add("Jason");
-        sampleStringList.add("Bolot");
-        sampleStringList.add("Sean");
-
-        return sampleStringList;
-    }
+            Intent linearLayoutManagerSampleIntent = LinearLayoutManagerSampleActivity.newIntent(context);
+            context.startActivity(linearLayoutManagerSampleIntent);
+        }
+    };
 }
